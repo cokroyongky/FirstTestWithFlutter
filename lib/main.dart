2c24d6704c9e9d123c0746b3cb4f5605bd170aff
C Yongky Pranowo <cokroyongkyp@Gmail.com>
2019-05-29 12:48:23 +0700

    Membuat TabView/TabBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,64 @@
+import './home.dart';
+import './popular.dart';
+import './news.dart';
 import 'package:flutter/material.dart';
 
-
-void main(){
-  runApp(HomePage());
+void main() {
+  runApp(CYPTabs());
 }
 
-class HomePage extends StatelessWidget{
-  build(context){
+class CYPTabs extends StatefulWidget{
+  @override
+  CYPTabState createState() => CYPTabState();
+}
+
+class CYPTabState extends State<CYPTabs> with SingleTickerProviderStateMixin{
+  
+  // Code Baru
+  TabController controller;
+
+  @override
+  void initState(){
+    super.initState();
+    controller = TabController(vsync: this, length: 3);
+  }
+
+  @override
+  void dispose(){
+    controller.dispose();
+    super.dispose();
+  }
+
+  // Akhir Code Baru
+  
+  
+  @override
+  Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
+          title: Text('CYP Tabs'),
           backgroundColor: Colors.red[800],
-          leading: Icon(Icons.home),
-          title: Text('DW Flutter Basic'),
-        ),
 
-        body: Container(
-          child: Card(
-            child: Column(
-              children: <Widget>[
-                Image.network('https://flutter.io/images/homepage/header-illustration.png'),
-                Text('Belajar Flutter')
-              ],
-            ),
+          // Code Baru
+          bottom: TabBar(
+            controller: controller,
+            tabs: <Tab>[
+              Tab(icon: Icon(Icons.home),),
+              Tab(icon: Icon(Icons.note),),
+              Tab(icon: Icon(Icons.poll),),
+            ]
           ),
         ),
-      )
+        body: TabBarView(
+          controller: controller,
+          children: <Widget>[
+            Home(),
+            Popular(),
+            News()
+          ]
+        )
+      ),
     );
   }
 }
